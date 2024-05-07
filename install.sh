39db83cac6d51acacb721f8f9915d6e33be6f942
= <=>
2024-05-07 19:33:45 -0300

    Improve some readbility of the script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 
 # Upgrade the system
 sudo dnf upgrade -y
 
-# Git
+# Install Git if not installed yet.
 sudo dnf install git -y
 
-# Install Brave Browser
+# Install Brave Browser.
 sudo dnf install dnf-plugins-core -y
 
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
@@ -15,12 +15,18 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 sudo dnf install brave-browser -y
 
-# Some system utils
-sudo dnf install fastfetch cronie gnome-tweaks gnome-console gnome-themes-extra -y
+# Install some system utils.
+sudo dnf install fastfetch cronie -y
 
+# Install some GNOME core apps (for me).
+if [[ "$DESKTOP_SESSION" != "plasma" ]]; then
+  sudo dnf install gnome-tweaks gnome-console gnome-themes-extra -y
+fi
+
+# Improve Fedora's max parallel download by increasing the limit to 10.
 sudo sh -c 'echo max_parallel_downloads=10 >> /etc/dnf/dnf.conf'
 
-# RPM Fusion
+# Add's RPM Fusion to install some proprietary softwares and essentials Codecs.
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf config-manager --enable fedora-cisco-openh264 -y
 sudo dnf groupupdate core -y
@@ -28,25 +34,30 @@ sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
 sudo dnf groupupdate multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 sudo dnf groupupdate sound-and-video -y
 
-# Flatpak
+# Install Non Free RPM Fusion Steam.
+sudo dnf install steam -y
+echo "@nClientDownloadEnableHTTP2PlatformLinux 0" > ~/.steam/steam/steam_dev.cfg
+
+# Install Flatpak and some cool apps.
 sudo dnf install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub info.febvre.Komikku -y
 flatpak install flathub com.usebruno.Bruno -y
-flatpak install flathub com.valvesoftware.Steam -y
 flatpak install flathub net.sapples.LiveCaptions -y
+flatpak install flathub com.spotify.Client -y
 
-# Fonts
+# Install JetBrains Mono Nerd fonts.
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip -d JetBrainsMono JetBrainsMono.zip
 sudo mv JetBrainsMono /usr/share/fonts
 rm JetBrainsMono.zip
 
-# Docker
+# Install Docker and add your user to docker group.
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo -y
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -54,6 +65,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 
 # HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -72,6 +84,7 @@ sudo dnf install fd-find -ripgrep y
 #LunarVim
 sudo dnf install neovim -y
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+echo 
 
 #Zsh
 sudo dnf install zsh -y
